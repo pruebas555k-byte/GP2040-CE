95893f7a022b880d7a2d9e12359bf3f65a983b79
pruebas555k-byte <pruebas555k@gmail.com>
2026-04-06 20:32:07 -0400

    Update gamepad_usb_host_listener.cpp

--- src/addons/gamepad_usb_host_listener.cpp
+++ src/addons/gamepad_usb_host_listener.cpp
@@ -7,7 +7,7 @@
 #include "drivers/ps4/PS4Descriptors.h"
 #include "drivers/ps4/PS4Driver.h"
 
-#define ANTI_RECOIL_STRENGTH 3000
+#define ANTI_RECOIL_STRENGTH 3600
 #define LED_EAFC_R 0xFF
 #define LED_EAFC_G 0xFF
 #define LED_EAFC_B 0xFF
@@ -308,7 +308,7 @@ void GamepadUSBHostListener::process_ds(uint8_t const* report, uint16_t len) {
                     macro_mute_start_time = getMillis();
                 }
                 if (macro_mute_active) {
-                    if (getMillis() - macro_mute_start_time < 480) {
+                    if (getMillis() - macro_mute_start_time < 486) {
                         _controller_host_state.buttons |= GAMEPAD_MASK_B3;
                         _controller_host_state.buttons |= GAMEPAD_MASK_B2;
                     } else {
